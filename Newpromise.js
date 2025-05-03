40d4cec8507f29fc6251001bfa449fe0d704a08f
Muhanguzi boss <bossmuhanguzi@gmail.com>
2025-05-03 11:47:54 +0200

    Error Handling with Promise.all method

--- Newpromise.js
+++ Newpromise.js
@@ -1,8 +1,10 @@
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, "foo");
-});
-Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log(values);
-});
+const promise1 = Promise.resolve(1);
+const promise2 = Promise.resolve(1);
+const promise3 = Promise.reject("Error!");
+Promise.all([promise1, promise2, promise3])
+  .then((results) => {
+    console.log("Results:", results);
+  })
+  .catch((error) => {
+    console.log("Uncaught error:", error);
+  });
